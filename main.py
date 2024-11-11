d568dc676eabf993deb03ac2410517ce7a2716a3
Servomouse <servomouse@protonmail.com>
2024-11-10 21:37:10 -0600

    Fix ws connection

--- main.py
+++ main.py
@@ -23,21 +23,21 @@ space_size = 100
 space =  [[[None for i in range(space_size)] for j in range(space_size)] for k in range(space_size)]
 
 genome = {
-    "mov_x": 0,
-    "mov_y": 0,
-    "mov_z": 0,
+    "move_x": 0,
+    "move_y": 0,
+    "move_z": 0,
     "split_dir": 'x',
     "right_child": None,
     "left_child": None,
     "neuron_id": 0
 }
 
-network_size = 32768
+network_size = 32   # 32768
 
 def assign_ids(nodes):
     for node in nodes:
         if node['left_child'] is None and node['right_child'] is None:
-            node['id'] = random.randint(128)
+            node['id'] = random.randint(0, 128)
 
 def create_node():
     node = {}
@@ -45,10 +45,11 @@ def create_node():
     node["move_y"] = random.randint(-5, 5)
     node["move_z"] = random.randint(-5, 5)
     dirs = ['x', 'y', 'z']
-    node["split_dir"] = dirs[random.randint(3)]
+    node["split_dir"] = dirs[random.randint(0, 2)]
     node["right_child"] = None
     node["left_child"] = None
     node["id"] = 0
+    return node
 
 def create_network(network_size):
     network_size = (network_size * 2) - 1
@@ -61,7 +62,7 @@ def create_network(network_size):
         if right_idx < network_size:
             nodes[i]['right_child'] = nodes[right_idx]
     assign_ids(nodes)
-    return nodes
+    return nodes[0]
 
 
 init_coords = [int(space_size/2), int(space_size/2), int(space_size/2)]
@@ -113,7 +114,7 @@ def move_node(coords, axis, direction):
 def process_node(coords):
     global space
     node = space[coords[0]][coords[1]][coords[2]]
-    new_coords = [coords[0] + node['mov_x'], coords[1] + node['mov_y'], coords[2] + node['mov_z']]
+    new_coords = [coords[0] + node['move_x'], coords[1] + node['move_y'], coords[2] + node['move_z']]
     if node["left_child"] is not None and node["right_child"] is not None:
         space[coords[0]][coords[1]][coords[2]] = None
         if node['split_dir'] == 'x':
@@ -158,6 +159,8 @@ def process_node(coords):
 def unfold_genome(genome, init_coords):
     global space
     space[init_coords[0]][init_coords[1]][init_coords[2]] = genome
+    print(f"{space[init_coords[0]][init_coords[1]][init_coords[2]] = }")
+    print(f"{space[init_coords[0]][init_coords[1]][init_coords[2]]['move_x'] = }")
     process_node(init_coords)
 
  
@@ -166,6 +169,8 @@ client_connection = None
 async_loop = None
 shutdown_event = None
 
+space_nodes = {}
+
 async def handler(websocket, path):
     global client_connection
     client_connection = websocket
@@ -173,6 +178,7 @@ async def handler(websocket, path):
         async for message in websocket:
             print(f"Received message: {message}")
             # await client_connection.send(json.dumps({'array': [1, 2, 3, 4, 5]}))
+            await client_connection.send(json.dumps(space_nodes))
     except websockets.ConnectionClosed:
         print("Client disconnected")
     finally:
@@ -197,18 +203,31 @@ def send_data(data):
     global client_connection, async_loop
     if client_connection:
         print(f"Sending {data = }")
+        # async_loop.run_until_complete(client_connection.send(data))
         async_loop.run_until_complete(client_connection.send(json.dumps(data)))
         # asyncio.run_coroutine_threadsafe(client_connection.send(data), async_loop)
 
 
 def main():
+    global space
     server_thread = threading.Thread(target=start_server)
     server_thread.start()
     try:
-        # unfold_genome(create_network(network_size), init_coords)
+        unfold_genome(create_network(network_size), init_coords)
+        for i in range(space_size):
+            for j in range(space_size):
+                for k in range(space_size):
+                    if space[i][j][k] is not None:
+                        space_nodes[f"{i}-{j}-{k}"] = {}
+                        space_nodes[f"{i}-{j}-{k}"]['coords'] = {}
+                        space_nodes[f"{i}-{j}-{k}"]['coords']['x'] = i
+                        space_nodes[f"{i}-{j}-{k}"]['coords']['y'] = j
+                        space_nodes[f"{i}-{j}-{k}"]['coords']['z'] = k
+                        space_nodes[f"{i}-{j}-{k}"]['color'] = '0x00FFFF'
+
         while True:
             time.sleep(1)
-            send_data({'array': [1, 2, 3, 4, 5]})
+            # send_data({'array': [1, 2, 3, 4, 5]})
     except KeyboardInterrupt:
         async_loop.call_soon_threadsafe(shutdown_event.set)
         server_thread.join()

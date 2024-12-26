117debdaf9893bc55c4b653927a1e67c68248403
Servomouse <servomouse@protonmail.com>
2024-12-25 19:32:27 -0600

    Update new_build.py

--- new_build.py
+++ new_build.py
@@ -7,16 +7,24 @@ common_flags = ["-lm", "-g", "-Wall", "-lws2_32"]
 
 targets = {
     "neuron_linear": {
+        "build_flags": common_flags + ["-shared"],
         "build_files": ["neuron_linear.c", "utils.c"],
-        "output_file": "neuron_linear.dll"
+        "output_file": "bin/neuron_linear.dll"
     },
     "neuron_smart": {
+        "build_flags": common_flags + ["-shared"],
         "build_files": ["neuron_smart.c", "utils.c"],
-        "output_file": "neuron_smart.dll"
+        "output_file": "bin/neuron_smart.dll"
     },
     "controller": {
-        "build_files": ["neuron_kapellmeister.c", "neuron_smart.c", "utils.c"],
-        "output_file": "controller.dll"
+        "build_flags": common_flags + ["-shared"],
+        "build_files": ["neuron_kapellmeister.c", "neuron_smart.c", "micro_net.c", "utils.c"],
+        "output_file": "bin/controller.dll"
+    },
+    "micronet_test": {
+        "build_flags": common_flags,
+        "build_files": ["micro_net_test.c", "neuron_smart.c", "micro_net.c", "utils.c"],
+        "output_file": "bin/micronet_test.exe"
     },
 }
 
@@ -25,13 +33,15 @@ def main(target):
     if target not in targets:
         print(f"Error: invalid target: {target}!")
         return
-    print(f"Building {target} . . . ", end='', flush=True)
-    fname = f"bin/{target}.dll"
+    fname = targets[target]["output_file"]
     if os.path.isfile(fname):
         os.remove(fname)
     srcs = ' '.join(targets[target]["build_files"])
-    flags = ' '.join(common_flags)
-    output = subprocess.getoutput(f"gcc -shared {flags} {srcs} -o {fname}")
+    flags = ' '.join(targets[target]["build_flags"])
+    cmd = f"gcc {flags} {srcs} -o {fname}"
+    print(cmd)
+    print(f"Building {target} . . . ", end='', flush=True)
+    output = subprocess.getoutput(cmd)
     if len(output) == 0:
         print("Done")
         return 0

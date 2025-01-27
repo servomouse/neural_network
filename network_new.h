d33e6fe48a8b587c3f13575349eac65c1fb31a7f
Servomouse <servomouse@protonmail.com>
2025-01-26 19:54:34 -0600

    network_new.c

--- network_new.h
+++ network_new.h
@@ -3,13 +3,18 @@
 #include <stdint.h>
 #include "neuron_types.h"
 
-void network_init(network_t * config, network_map_t *net_map, uint32_t dataset_size, double **bckp_coeffs);
+void network_init(network_t * config, network_map_t *net_map, uint32_t dataset_size);
 double *network_get_outputs(network_t * config, double *inputs, uint32_t to_print);
-void network_set_global_error(network_t * config, double error);
+void network_set_local_errors(network_t * config, double *errors, uint32_t offset);
+void network_set_global_error(network_t * config, double error, uint32_t offset);
 void network_backup(network_t * config);
 void network_check_backup(network_t * config);
 void network_restore(network_t * config);
-void network_update(network_t * config);
+
+void network_stash_neurons(network_t * config);
+void network_update_neurons(network_t * config);
+void network_rollback_neurons(network_t * config);
+
 void network_reset_counters(network_t *config);
 
 void network_mutate(network_t * config);
@@ -21,3 +26,14 @@ void network_save_data(network_t * config, char *filename);
 void network_restore_data(network_t * config, char *filename);
 
 void network_print_coeffs(network_t * config);
+
+
+/*
+    network_mutate_micronet();
+    network_stash_neurons();
+    for(int i=0; i<10; i++)
+        network_update_neurons();
+    [measure_error]
+    network_rollback_neurons();
+    optional: network_rollback_micronet();
+*/

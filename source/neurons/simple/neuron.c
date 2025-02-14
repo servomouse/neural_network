034c77b75254b19ee6be0d566bb6c0b22bcbed38
Servomouse <servomouse@protonmail.com>
2025-02-13 19:39:29 -0600

    Merge simple and linear neurons code

--- source/neurons/simple/neuron.c
+++ source/neurons/simple/neuron.c
@@ -26,21 +26,28 @@ static  double control_coeffs_func(double coeff) {
 }
 
 static void * alloc_memory(void *p, size_t num_elements, size_t sizeof_element) {
-    if(p) {
-        free(p);
-    }
+    if(p) { free(p); }
     return calloc(num_elements, sizeof_element);
 }
 
-void neuron_init(neuron_params_t * n_params, uint32_t num_inputs) {
+void neuron_init(neuron_params_t * n_params, neuron_type_t n_type, uint32_t num_inputs) {
     // srand(time(NULL));   // Should be called by controller
     // printf("Creating simple neuron with %d inputs\n", num_inputs);
+    n_params->n_type = n_type;
     n_params->num_inputs = num_inputs;
-    n_params->num_coeffs = 1 << num_inputs;
+    if(n_type == NLinear) {
+        n_params->num_coeffs = num_inputs+1;    // +1 for BIAS
+    } else if(n_type == NPoly) {
+        n_params->num_coeffs = 1 << num_inputs;
+    } else {
+        printf("Error: unknown neuron type: %d; exit\n", n_type);
+        exit(1);
+    }
     n_params->mutation_step = 0.01;
 
     n_params->inputs = alloc_memory(n_params->inputs, n_params->num_inputs, sizeof(double));
-    n_params->indices = alloc_memory(n_params->indices, num_inputs, sizeof(uint32_t));
+    n_params->indices = alloc_memory(n_params->indices, n_params->num_inputs, sizeof(uint32_t));
+
     n_params->last_vector = alloc_memory(n_params->last_vector, n_params->num_coeffs, sizeof(double));
     n_params->rand_vector = alloc_memory(n_params->rand_vector, n_params->num_coeffs, sizeof(double));
     n_params->coeffs = alloc_memory(n_params->coeffs, n_params->num_coeffs, sizeof(double));
@@ -53,9 +60,9 @@ void neuron_set_input_idx(neuron_params_t *n_params, uint32_t input_number, uint
     // printf("Setting input %d index to %d\n", input_number, input_idx);
     if(input_number < n_params->num_inputs) {
         n_params->indices[input_number] = input_idx;
-    } else {
-        printf("ERROR: index out of range: input_number = %d, network size = %d\n", input_number, n_params->num_inputs);
+        return;
     }
+    printf("ERROR: index out of range: input_number = %d, network size = %d\n", input_number, n_params->num_inputs);
 }
 
 void neuron_set_coeffs(neuron_params_t * n_params, double *coeffs) {
@@ -78,9 +85,6 @@ uint32_t neuron_get_num_coeffs(neuron_params_t * n_params) {
 
 // Returns number of bytes written to the buffer
 int neuron_get_coeffs_as_string(neuron_params_t *n_params, char *buffer, uint32_t buffer_size) {
-    // for(uint32_t i=0; i<buffer_size; i++) {
-    //     buffer[i] = 0;
-    // }
     uint32_t idx = 0;
     for(uint32_t i=0; i<n_params->num_coeffs-1; i++) {
         idx += snprintf(&buffer[idx], buffer_size-idx, "\t%.5f,\n", n_params->coeffs[i]);
@@ -90,23 +94,31 @@ int neuron_get_coeffs_as_string(neuron_params_t *n_params, char *buffer, uint32_
 }
 
 double neuron_get_output(neuron_params_t *n_params, double *inputs) {
-    double output = n_params->coeffs[0];         // BIAS
-    for(size_t i=1; i<n_params->num_coeffs; i++) {
-        double temp = 1.0;
-        for(size_t j=0; j<n_params->num_inputs; j++) {
-            if(((1 << j) & i)> 0) {
-                uint32_t idx = n_params->indices[j];
-                temp *= inputs[idx];
-            }
+    double output;
+    if(n_params->n_type == NLinear) {
+        output = n_params->coeffs[n_params->num_inputs];         // BIAS
+        for(size_t i=0; i<n_params->num_inputs; i++) {
+            uint32_t idx = n_params->indices[i];
+            output += inputs[idx] * n_params->coeffs[i];
         }
-        output += temp * n_params->coeffs[i];
+    } else if(n_params->n_type == NPoly) {
+        output = n_params->coeffs[0];         // BIAS
+        for(size_t i=1; i<n_params->num_coeffs; i++) {
+            double temp = 1.0;
+            for(size_t j=0; j<n_params->num_inputs; j++) {
+                if(((1 << j) & i)> 0) {
+                    uint32_t idx = n_params->indices[j];
+                    temp *= inputs[idx];
+                }
+            }
+            output += temp * n_params->coeffs[i];
+        }
+    } else {
+        printf("Error: unknown neuron type: %d; exit\n", n_params->n_type);
+        exit(1);
     }
     return activation_func(output);
 }
-
-// uint32_t neuron_get_num_coeffs(neuron_params_t * n_params) {
-//     return n_params->num_coeffs;
-// }
 
 void neuron_stash_state(neuron_params_t * n_params) {
     for(uint32_t i=0; i<n_params->num_coeffs; i++) {
--- source/neurons/simple/neuron.h
+++ source/neurons/simple/neuron.h
@@ -5,8 +5,8 @@
 #include <stdlib.h>
 #include "neuron_types.h"
 
-void neuron_init(neuron_params_t * n_params, uint32_t num_inputs);
-double neuron_get_output(neuron_params_t * n_params, double *inputs);
+void neuron_init(neuron_params_t * n_params, neuron_type_t n_type, uint32_t num_inputs);
+double neuron_get_output(neuron_params_t *n_params, double *inputs);
 void neuron_set_input_idx(neuron_params_t * n_params, uint32_t input_number, uint32_t input_idx);
 
 void neuron_set_coeffs(neuron_params_t * n_params, double *coeffs);

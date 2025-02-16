f66c628dede7d2e12ffcf9216cb7a0d6cdaa5084
Servomouse <servomouse@protonmail.com>
2025-02-16 16:09:45 -0600

    Update neuron_simple_test

--- tests/neuron_simple_test.c
+++ tests/neuron_simple_test.c
@@ -72,23 +72,28 @@ int test_rollback(neuron_params_t *n) {
 
 int test_save_restore(neuron_params_t *n) {
     double init_error = get_error(n, polynome_dataset, sizeof_arr(polynome_dataset), 0);
-    uint32_t num_coeffs = neuron_get_num_coeffs(n);
-    double *bckp_coeffs = calloc(num_coeffs, sizeof(double));
-    for(uint32_t i=0; i<num_coeffs; i++) {
-        bckp_coeffs[i] = neuron_get_coeff(n, i);
-    }
-    // neuron_save_coeffs(n, 123);
+    // uint32_t num_coeffs = neuron_get_num_coeffs(n);
+    // double *bckp_coeffs = calloc(num_coeffs, sizeof(double));
+    // for(uint32_t i=0; i<num_coeffs; i++) {
+    //     bckp_coeffs[i] = neuron_get_coeff(n, i);
+    // }
+    neuron_save(n, "neuron_backup.bin");
     neuron_set_coeff(n, 0, 0.5);
     neuron_set_coeff(n, 1, 0.5);
     neuron_set_coeff(n, 2, 0.5);
     neuron_set_coeff(n, 3, 0.5);
-    // double init_error = get_error(n, dataset, sizeof_arr(dataset), 0);
+    double new_error = get_error(n, polynome_dataset, sizeof_arr(polynome_dataset), 0);
+    if(init_error == new_error) {
+        printf("Error: save-restore doesn't work: error after modification == init error!\n");
+        return EXIT_FAILURE;
+    }
+
     // printf("Error after setting coeffs = %f;\n", error);
-    // neuron_restore_data(n, 123);
-    neuron_set_coeffs(n, bckp_coeffs);
+    neuron_restore(n, "neuron_backup.bin");
+    // neuron_set_coeffs(n, bckp_coeffs);
     double error = get_error(n, polynome_dataset, sizeof_arr(polynome_dataset), 0);
     if(init_error != error) {
-        printf("Error: save-restore doesn't work: error after restore != error before save");
+        printf("Error: save-restore doesn't work: error after restore != error before modification");
         return EXIT_FAILURE;
     }
     // printf("Error after restore = %f;\n", error);

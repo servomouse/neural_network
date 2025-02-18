cf67b9a0b2d0822ceb04e5230e338333b1e9e032
Servomouse <servomouse@protonmail.com>
2025-02-17 19:26:47 -0600

    Update micronet to work with new neuron implementation

--- source/micronet/micronet.h
+++ source/micronet/micronet.h
@@ -3,6 +3,35 @@
 #include <stdint.h>
 #include "neuron_types.h"
 
+typedef struct {
+    uint32_t idx;
+    uint32_t num_inputs;
+    neuron_type_t n_type;
+    uint32_t indices[0];
+} subneuron_description_t;
+
+typedef struct {
+    uint32_t num_inputs;
+    uint32_t num_neurons;
+    uint32_t net_size;
+    uint32_t *neurons;  // subneuron_description_t is used here
+    uint32_t num_outputs;
+    uint32_t output_indices[];
+} micronet_map_t;
+
+typedef struct {
+    uint32_t num_inputs;
+    uint32_t num_neurons;
+    uint32_t net_size;
+    uint32_t num_outputs;
+    uint32_t *output_indices;
+    double *arr;
+    double *outputs;
+    neuron_params_t *neurons;
+    uint32_t mutated_neuron_idx;
+    micronet_map_t *map;
+} micro_network_t;
+
 void micronet_init(micro_network_t * config, micronet_map_t *net_map, double **bckp_coeffs);
 double *micronet_get_output(micro_network_t *config, double *inputs);
 void micronet_save(micro_network_t * config, char *filename);

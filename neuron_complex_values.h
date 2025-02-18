b2e026cc1c42af9da04318876500fc71d6ba9705
Servomouse <servomouse@protonmail.com>
2025-02-17 19:50:03 -0600

    Clean up the project

--- neuron_complex_values.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "neuron.h"
-
-void neuron_add_complex_item_value(complex_item_t *item, double value);
-void neuron_clear_complex_items(complex_item_t *items, uint32_t num_elements);
-double neuron_get_complex_item_value(complex_item_t *item);
-void neuron_sub_complex_item_value(complex_item_t *item, double value);

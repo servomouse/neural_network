ff809df87cd5df9a94b9eaabb6295b730cfb601c
Servomouse <servomouse@protonmail.com>
2025-02-22 17:40:41 -0600

    Add test for micronet save-restore functionality

--- source/micronet/micronet_save_restore.c
+++ source/micronet/micronet_save_restore.c
@@ -2,8 +2,37 @@
 #include "utils.h"
 #include "neuron.h"
 #include "micronet_common.h"
+#include <string.h>
 
 #define BUF_SIZE 256
+
+static void prepare_folders(char *path) {
+    char buf[BUF_SIZE] = {0};
+    snprintf(buf, BUF_SIZE, "mkdir -p %s/neurons", path);
+
+    /* Special preprocessing for windows */
+    #ifdef _WIN32   // Update path to make it work in windows
+        char buf2[BUF_SIZE] = {0};
+        uint32_t b2_pos = 0;
+        for(uint32_t i=0; i<BUF_SIZE; i++) {
+            if(buf[i] == 0) {
+                break;
+            } else if(buf[i] == '/') {
+                buf2[b2_pos++] = '\\';
+                buf2[b2_pos++] = '\\';
+            } else {
+                buf2[b2_pos++] = buf[i];
+            }
+        }
+        clear_buffer(buf, BUF_SIZE);
+        strcpy(buf, buf2);
+    #endif
+    /* !Special preprocessing for windows */
+
+    printf("Creating folder: %s, ", buf2);
+    int ret_val = system(buf2);
+    printf("result: %d\n", ret_val);
+}
 
 static void save_neurons(micro_network_t * config, char *path) {
     char buf[BUF_SIZE];
@@ -17,6 +46,7 @@ static void save_neurons(micro_network_t * config, char *path) {
 }
 
 void micronet_save(micro_network_t * config, char *path) {
+    prepare_folders(path);
     char *map_path = concat_strings(path, "/map.bin");
     char *neurons_path = concat_strings(path, "/neurons.bin");
     // char *neurons_path = concat_strings(path, "/neurons");
---
+++ tests/micronet/micronet_test_save_restore.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "neuron.h"
+#include "micronet.h"
+#include "utils.h"
+
+uint32_t linear_neurons[] = {
+    // idx  num_inputs  type indices
+       4,   4,          0,   0, 1, 2, 3,
+       5,   4,          0,   0, 1, 2, 3,
+       6,   4,          0,   0, 1, 2, 3,
+       7,   4,          0,   0, 1, 2, 3,
+       8,   4,          0,   4, 5, 6, 7,
+};
+
+micronet_map_t linear_micronet_map = {
+    .num_inputs = 4,
+    .num_neurons = 5,
+    .net_size = 9,
+    .neurons = linear_neurons,
+    .num_outputs = 1,
+    .output_indices = {8},
+};
+
+double linear_coeffs[5][5] = {  // Plus offset
+    {-0.60, -0.75, -0.10, 0.40, 0.0},
+    {0.40, -0.70, 0.35, 0.20, 0.70},
+    {0.70, 0.60, -0.50, 0.60, 0.60},
+    {0.0, -0.25, 0.70, -0.60, 0.60},
+    {0.75, 0.15, 0.75, 0.60, -0.60},
+};
+
+typedef struct {
+    double inputs[4];
+    double outputs[1];
+} dataset_entry_t;
+
+dataset_entry_t linear_dataset[] = {
+    {.inputs = {0.199320, -0.668140, 0.777210, 0.263040}, .outputs = {0.537650}},
+    {.inputs = {0.288430, -0.081390, -0.556510, 0.019750}, .outputs = {0.347243}},
+    {.inputs = {0.645860, -0.719660, 0.106780, -0.368080}, .outputs = {0.405290}},
+    {.inputs = {-0.539540, 0.511760, -0.500110, -0.149140}, .outputs = {-0.019801}},
+    {.inputs = {0.627920, 0.477830, 0.372300, -0.605580}, .outputs = {-0.073561}},
+    {.inputs = {0.097260, 0.023590, -0.888120, -0.670030}, .outputs = {0.018065}},
+    {.inputs = {0.972110, -0.078710, 0.152930, -0.898070}, .outputs = {-0.060608}},
+    {.inputs = {-0.322370, -0.381210, -0.019070, -0.233310}, .outputs = {0.313155}},
+    {.inputs = {0.507070, 0.851250, 0.547840, 0.996400}, .outputs = {-0.090780}},
+    {.inputs = {0.858700, -0.821960, 0.910280, -0.584640}, .outputs = {-0.091233}},
+    {.inputs = {0.477830, -0.490830, 0.902340, 0.830680}, .outputs = {0.821575}},
+    {.inputs = {-0.985660, 0.680960, -0.343490, -0.079930}, .outputs = {-0.109616}},
+    {.inputs = {-0.790400, -0.854120, 0.784660, -0.060950}, .outputs = {0.229011}},
+    {.inputs = {0.024690, 0.838620, -0.968810, 0.010280}, .outputs = {-0.465611}},
+    {.inputs = {-0.977840, -0.721120, 0.212560, 0.557420}, .outputs = {0.439960}},
+    {.inputs = {0.315040, 0.976870, -0.747490, 0.445910}, .outputs = {-0.616961}},
+};
+
+static double get_error(micro_network_t *net, dataset_entry_t *dataset, size_t dataset_size, uint32_t num_outputs) {
+    // Works only with single output networks
+    double error = 0;
+    for(size_t i=0; i<dataset_size; i++) {
+        double *outputs = micronet_get_output(net, dataset[i].inputs);
+        double e = 0.0;
+        for(uint32_t j=0; j<num_outputs; j++) {
+            double delta = dataset[i].outputs[j] - outputs[j];
+            e += delta * delta;
+        }
+
+        // printf("Desired outputs: [");
+        // for(uint32_t j=0; j<num_outputs-1; j++) {
+        //     printf("%f, ", dataset[i].outputs[j]);
+        // }
+        // printf("%f], ", dataset[i].outputs[num_outputs-1]);
+
+        // printf("real outputs: [");
+        // for(uint32_t j=0; j<num_outputs-1; j++) {
+        //     printf("%f, ", outputs[j]);
+        // }
+        // printf("%f];\n", outputs[num_outputs-1]);
+
+        error += e;
+    }
+    return error / dataset_size;
+}
+
+void init_coeffs(micro_network_t *net) {
+    for(uint32_t i=0; i<5; i++) {
+        // printf("{");
+        for(uint32_t j=0; j<5; j++) {
+            // double coeff = random_double(-0.75, 0.75);
+            // printf("%f, ", coeff);
+            neuron_set_coeff(&net->neurons[i], j, linear_coeffs[i][j]);
+        }
+        // printf("},\n");
+    }
+}
+
+int main(void) {
+    srand(time(NULL));
+    micro_network_t target_net = {0};
+    micronet_init(&target_net, &linear_micronet_map);
+    init_coeffs(&target_net);
+    printf("Target microNet initialised!\n");
+
+    double init_error = get_error(&target_net, linear_dataset, sizeof_arr(linear_dataset), 1);
+    printf("Init error: %f\n", init_error);
+    if(round_to_precision(init_error, 5) != round_to_precision(0.0, 5)) {
+        printf("Error: initial error is not equal to 0!\n");
+        return EXIT_FAILURE;
+    }
+    char *n_path = concat_strings(BCKP_DIR_PATH, "/target_unet");
+    micronet_save(&target_net, n_path);
+    
+    for(uint32_t i=0; i<5; i++) {
+        for(uint32_t j=0; j<5; j++) {
+            neuron_set_coeff(&target_net.neurons[i], j, 0.1);
+        }
+    }
+    init_error = get_error(&target_net, linear_dataset, sizeof_arr(linear_dataset), 1);
+    printf("Error after save: %f\n", init_error);
+    if(round_to_precision(init_error, 5) == round_to_precision(0.0, 5)) {
+        printf("Error: after messing up coeffitients error is still 0!\n");
+        free(n_path);
+        return EXIT_FAILURE;
+    }
+    micronet_restore(&target_net, n_path);
+    free(n_path);
+    init_error = get_error(&target_net, linear_dataset, sizeof_arr(linear_dataset), 1);
+    printf("Error after restore: %f\n", init_error);
+    if(round_to_precision(init_error, 5) != round_to_precision(0.0, 5)) {
+        printf("Error: after restoring error is not equal to 0!\n");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}

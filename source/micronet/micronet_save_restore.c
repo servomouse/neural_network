99789fff264aab988cd7d7c9b852ceb66ca8309c
Servomouse <servomouse@protonmail.com>
2025-02-28 20:36:46 -0600

    Update save-restore methods

--- source/micronet/micronet_save_restore.c
+++ source/micronet/micronet_save_restore.c
@@ -6,31 +6,53 @@
 
 #define BUF_SIZE 256
 
+/* Updates path string in place */
+static void prepare_path_for_windows(char *path_string) {
+    char buf2[BUF_SIZE] = {0};
+    uint32_t b2_pos = 0;
+    for(uint32_t i=0; i<BUF_SIZE; i++) {
+        if(path_string[i] == 0) {
+            break;
+        } else if(path_string[i] == '/') {
+            buf2[b2_pos++] = '\\';
+            buf2[b2_pos++] = '\\';
+        } else {
+            buf2[b2_pos++] = path_string[i];
+        }
+    }
+    clear_buffer(path_string, BUF_SIZE);
+    strcpy(path_string, buf2);
+}
+
 static void prepare_folders(char *path) {
     char buf[BUF_SIZE] = {0};
     snprintf(buf, BUF_SIZE, "mkdir -p %s/neurons", path);
 
     /* Special preprocessing for windows */
     #ifdef _WIN32   // Update path to make it work in windows
-        char buf2[BUF_SIZE] = {0};
-        uint32_t b2_pos = 0;
-        for(uint32_t i=0; i<BUF_SIZE; i++) {
-            if(buf[i] == 0) {
-                break;
-            } else if(buf[i] == '/') {
-                buf2[b2_pos++] = '\\';
-                buf2[b2_pos++] = '\\';
-            } else {
-                buf2[b2_pos++] = buf[i];
-            }
-        }
-        clear_buffer(buf, BUF_SIZE);
-        strcpy(buf, buf2);
+        prepare_path_for_windows(buf);
     #endif
     /* !Special preprocessing for windows */
 
-    printf("Creating folder: %s, ", buf2);
-    int ret_val = system(buf2);
+    printf("Creating folder: %s, ", buf);
+    int ret_val = system(buf);
+    printf("result: %d\n", ret_val);
+}
+
+void remove_folders(char *path) {
+    char buf[BUF_SIZE] = {0};
+    snprintf(buf, BUF_SIZE, "rd _s _q \"%s\"", path);
+
+    /* Special preprocessing for windows */
+    #ifdef _WIN32   // Update path to make it work in windows
+        prepare_path_for_windows(buf);
+    #endif
+    /* !Special preprocessing for windows */
+    buf[3] = '/';
+    buf[6] = '/';
+
+    printf("Removing folder: %s, ", buf);
+    int ret_val = system(buf);
     printf("result: %d\n", ret_val);
 }
 
--- source/micronet/micronet_save_restore.h
+++ source/micronet/micronet_save_restore.h
@@ -4,3 +4,4 @@
 
 void micronet_save(micro_network_t * config, char *path);
 void micronet_restore(micro_network_t * config, char *path);
+void remove_folders(char *path);

2967dcbf0b15f7df239e3828d1ccadae548de204
Servomouse <servomouse@protonmail.com>
2025-03-05 20:39:04 -0600

    Continue implementing NADL parser

--- nadl_parser.py
+++ nadl_parser.py
@@ -90,11 +90,93 @@ def parse_item(string):
             return items
 
 
+def prepare_data(data):
+    config = []
+    counter = 0
+    for l in data.split("\n"):
+        temp = l.strip()
+        if temp.startswith("#") or len(temp) == 0:
+            counter += 1
+            continue
+        elif '#' in temp:
+            temp = temp.split("#")[0].strip()
+        config.append({"line_number": counter, "data": temp})
+        counter += 1
+    return config
+
+
+def parse_module_name(data):
+    m_name = data[0]["data"].split("module")[1].strip()
+    m_name = m_name.split(' ')
+    syntax_error_line = f"Error: invalid syntax at line {data[0]['line_number']}: \"{data[0]['data']}\"\n" +\
+                        "\tCorrect syntax looks like this: \"module: modulename\""
+    if len(m_name) > 2:
+        raise Exception(syntax_error_line)
+        sys.exit(1)
+    elif len(m_name) == 2:
+        if m_name[0] == ':':
+            if m_name[1][0] in symbols:
+                return m_name[1], data[1:]
+            else:
+                raise Exception(f"Error: Invalid module name: {m_name[1]};\n"
+                                f"\tModule name should start with a symbol, but it starts with {m_name[1][0]}")
+    elif len(m_name) == 1:
+        if m_name[0][0] == ':':
+            if len(m_name[0]) == 1:
+                raise Exception(syntax_error_line)
+            if m_name[0][1] in symbols:
+                return m_name[0][1:], data[1:]
+            else:
+                raise Exception(f"Error: Invalid module name: {m_name[0][1:]};\n"
+                                f"\tModule name should start with a symbol, but it starts with {m_name[0][1]}")
+        else:
+            raise Exception(syntax_error_line)
+    else:
+        raise Exception(syntax_error_line)
+
+
+def parse_inputs(data):
+    if "inputs" not in data[0]['data']:
+        raise Exception(f"Invalid syntax at line {data[0]['line_number']}: {data[0]['data']}")
+    temp = data[0]['data'].split('inputs')
+    if len(temp[0]) > 0:
+        raise Exception(f"Invalid syntax at line {data[0]['line_number']}: {data[0]['data']}")
+    temp = temp[1].strip()
+    temp = temp.split(':')
+    if len(temp[0]) > 0:
+        raise Exception(f"Invalid syntax at line {data[0]['line_number']}: {data[0]['data']}")
+    temp = temp[1].strip()
+    print(temp)
+    sys.exit(0)
+
+
+def parse_module(data):
+    module_name = None
+    while len(data) > 0:
+        if "module" in data[0]['data']:
+            module_name, data = parse_module_name(data)
+            print(f"{module_name = }")
+            inputs, data = parse_inputs(data)
+        elif "inputs" in data[0]['data']:
+            inputs, data = parse_inputs(data)
+        else:
+            raise Exception(f"Invalid syntax: at line {data[0]['line_number']}: {data[0]['data']}\n"
+                            "Start config with \"module: modulename\" or \"inputs: [\"")
+
+
+def parse_modules(data):
+    modules = {}
+    mod_name, mod_config, data = parse_module(data)
+
+
 def parse(filename):
     with open(filename) as f:
         data = f.read()
-    config = parse_item(data)
-    print(config)
+    # config = parse_item(data)
+    data = prepare_data(data)
+    modules = parse_modules(data)
+    # for l in data:
+    #     print(l)
 
 
 if __name__ == "__main__":

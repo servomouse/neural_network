037d4f449faf8b0cac1a83cb1259cebdc9c3e192
servomouse <servomouse@protonmail.com>
2025-05-24 08:06:19 -0600

    Update poly neuron mutations source

--- source/neuron/neuron_mutations.c
+++ source/neuron/neuron_mutations.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "utils.h"
 #include "neuron.h"
+#include "poly_neuron.h"
 
 static double control_coeffs_func(double coeff) {
     if(coeff > 1.0) {
@@ -17,37 +18,49 @@ static double control_coeffs_func(double coeff) {
 
 // The opposite is neuron_rollback
 void neuron_stash_state(neuron_params_t * n_params) {
-    for(uint32_t i=0; i<n_params->num_coeffs; i++) {
-        n_params->last_vector[i] = n_params->coeffs[i];
+    if(n_params->n_type == NPoly) {
+        neuron_poly_stash_state(n_params);
+    } else {
+        for(uint32_t i=0; i<n_params->num_coeffs; i++) {
+            n_params->last_vector[i] = n_params->coeffs[i];
+        }
     }
 }
 
 void neuron_mutate(neuron_params_t * n_params) {
-    neuron_stash_state(n_params);
-    
-    if(n_params->mutated == 1) {    // If previuos mutation was successfull, keep going in the same direction
-        n_params->bad_mutations_counter = 0;
+    if(n_params->n_type == NPoly) {
+        neuron_poly_mutate(n_params);
     } else {
-        if(n_params->bad_mutations_counter >= 10000) {
-            // If there was many unsuccessfull mutations, try a large mutation
-            gen_vector(n_params->num_coeffs, random_double(0, 1), n_params->rand_vector);
+        neuron_stash_state(n_params);
+        
+        if(n_params->mutated == 1) {    // If previuos mutation was successfull, keep going in the same direction
+            n_params->bad_mutations_counter = 0;
         } else {
-            gen_vector(n_params->num_coeffs, random_double(0, n_params->mutation_step), n_params->rand_vector);
+            if(n_params->bad_mutations_counter >= 10000) {
+                // If there was many unsuccessfull mutations, try a large mutation
+                gen_vector(n_params->num_coeffs, random_double(0, 1), n_params->rand_vector);
+            } else {
+                gen_vector(n_params->num_coeffs, random_double(0, n_params->mutation_step), n_params->rand_vector);
+            }
         }
+        for(uint32_t i=0; i<n_params->num_coeffs; i++) {
+            n_params->coeffs[i] = control_coeffs_func(n_params->coeffs[i] + n_params->rand_vector[i]);
+        }
+        n_params->mutated = 1;
     }
-    for(uint32_t i=0; i<n_params->num_coeffs; i++) {
-        n_params->coeffs[i] = control_coeffs_func(n_params->coeffs[i] + n_params->rand_vector[i]);
-    }
-    n_params->mutated = 1;
 }
 
 // The opposite is neuron_stash_state
 void neuron_rollback(neuron_params_t * n_params) {
-    for(uint32_t i=0; i<n_params->num_coeffs; i++) {
-        n_params->coeffs[i] = n_params->last_vector[i];
-    }
-    if(n_params->mutated == 1) {
-        n_params->bad_mutations_counter ++;
-        n_params-> mutated = 0;
+    if(n_params->n_type == NPoly) {
+        neuron_poly_rollback(n_params);
+    } else {
+        for(uint32_t i=0; i<n_params->num_coeffs; i++) {
+            n_params->coeffs[i] = n_params->last_vector[i];
+        }
+        if(n_params->mutated == 1) {
+            n_params->bad_mutations_counter ++;
+            n_params-> mutated = 0;
+        }
     }
 }
--- source/neuron/poly/poly_neuron.h
+++ source/neuron/poly/poly_neuron.h
@@ -7,3 +7,8 @@
 
 void neuron_poly_init(neuron_params_t * n_params, uint32_t num_inputs);
 double neuron_poly_get_output(neuron_params_t *n_params, double *inputs);
+
+// Mutations:
+void neuron_poly_stash_state(neuron_params_t * n_params);
+void neuron_poly_mutate(neuron_params_t * n_params);
+void neuron_poly_rollback(neuron_params_t * n_params);

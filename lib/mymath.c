cd7856621af3d15a81056dd9c6eefdc57bda6dfb
servomouse <servomouse@protonmail.com>
2025-05-25 12:26:46 -0600

    Create the mymath library

---
+++ lib/mymath.c
@@ -0,0 +1,118 @@
+#include <math.h>
+#include "utils.h"
+
+/* generate a random double number from min to max */
+double random_double(double min, double max) {
+    // Make sure you have called srand(time(NULL));
+    double range = (max - min); 
+    double div = RAND_MAX / range;
+    return round_to_precision(min + (rand() / div), 5);
+}
+
+uint8_t random_bit(void) {
+    return rand() & 1;
+}
+
+uint8_t count_bits(size_t value) {
+    uint8_t counter = 0;
+    for(uint8_t i=0; i<(8*sizeof(size_t)); i++) {
+        if(value & ((size_t)1<<i)) {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+// Returns a random integer, min values is min, max value is max-1
+uint32_t random_int(uint32_t min, uint32_t max) {
+    uint32_t range = (max - min); 
+    // uint32_t div = RAND_MAX / range;
+    return min + (rand() % range);
+}
+
+// Generates a random vector of given length in an n-dimensional space
+void gen_vector(uint32_t n, double len, double* result) {
+    // Generate random components
+    double sum_of_squares = 0.0;
+    for (uint32_t i = 0; i < n; ++i) {
+        result[i] = ((double)rand() / RAND_MAX) * 2.0 - 1.0; // Random number between -1 and 1
+        sum_of_squares += result[i] * result[i];
+    }
+
+    // Normalize the vector to have unit length
+    double norm = sqrt(sum_of_squares);
+    for (uint32_t i = 0; i < n; ++i) {
+        result[i] /= norm;
+    }
+
+    // Scale the vector to the desired length
+    for (uint32_t i = 0; i < n; ++i) {
+        result[i] *= len;
+    }
+
+    // Cut off the trailing digits
+    for (uint32_t i = 0; i < n; ++i) {
+        result[i] = round_to_precision(result[i], 5);
+    }
+}
+
+static double complex_vector_length(uint32_t n, void* vector_2d) {
+    typedef struct {
+        double val[2];
+    } complex_vector_t;
+    complex_vector_t *vector = (complex_vector_t*)vector_2d;
+    double sum_of_squares = 0.0;
+    for (uint32_t i=0; i<n; i++) {
+        sum_of_squares += vector[i].val[0] * vector[i].val[0] + vector[i].val[1] * vector[i].val[1];
+    }
+    double norm = sqrt(sum_of_squares);
+    return norm;
+}
+
+// Generates a random 2d vector (a complex vector) of given length in an n-dimensional space
+void gen_complex_vector(uint32_t n, double len, void* result) {
+    typedef struct {
+        double val[2];
+    } complex_vector_t;
+    complex_vector_t *vector = (complex_vector_t*)result;
+    // Generate random components
+    for(uint32_t i=0; i<n; i++) {
+            vector[i].val[0] = ((double)rand() / RAND_MAX) * 2.0 - 1.0; // Random number between -1 and 1
+            vector[i].val[1] = ((double)rand() / RAND_MAX) * 2.0 - 1.0; // Random number between -1 and 1
+    }
+
+    // Get length
+    double norm = complex_vector_length(n, vector);
+
+    // Normalize the vector to have unit length
+    for (uint32_t i = 0; i < n; ++i) {
+        vector[i].val[0] /= norm;
+        vector[i].val[1] /= norm;
+    }
+
+    // Scale the vector to the desired length
+    for (uint32_t i = 0; i < n; ++i) {
+        vector[i].val[0] *= len;
+        vector[i].val[1] *= len;
+    }
+
+    // Cut off the trailing digits
+    for (uint32_t i = 0; i < n; ++i) {
+        vector[i].val[0] = round_to_precision(vector[i].val[0], 5);
+        vector[i].val[1] = round_to_precision(vector[i].val[1], 5);
+    }
+}
+
+// Precision is how many digits after the decimal point
+double round_to_precision(double value, uint32_t precision) {
+    double factor = pow(10.0, precision);
+    return round(value*factor)/factor;
+}
+
+uint64_t get_hash(uint8_t *data, size_t size) {
+    uint64_t hash = 5381;
+    for(size_t i=0; i<size; i++) {
+        hash = ((hash << 5) + hash) + data[i];
+    }
+    return hash;
+}
--- lib/utils.h
+++ lib/utils.h
@@ -12,24 +12,29 @@
 
 #define sizeof_arr(_x) sizeof(_x)/sizeof(_x[0])
 
-double random_double(double min, double max);
-uint8_t random_bit(void);
-uint32_t random_int(uint32_t min, uint32_t max);
+void sleep_ms(int ms);
+void sleep_s(int s);
 int store_data(void *data, size_t size, char *filename);
 void *restore_data(char *filename);
 char *concat_strings(const char *s1, const char *s2);
-void gen_vector(uint32_t n, double len, double* result);double round_to_precision(double value, uint32_t precision);
-uint8_t count_bits(size_t value);
 void write_buf_to_file(const char *buffer, const char *filename);
 void clear_buffer(char *buffer, uint32_t buffer_size);
+void * alloc_memory(void *p, size_t num_elements, size_t sizeof_element);
 
-void sleep_ms(int ms);
-void sleep_s(int s);
+// mymath.c:
+double random_double(double min, double max);
+uint8_t random_bit(void);
+uint32_t random_int(uint32_t min, uint32_t max);
+void gen_vector(uint32_t n, double len, double* result);
+void gen_complex_vector(uint32_t n, double len, void* result);
+double round_to_precision(double value, uint32_t precision);
+uint8_t count_bits(size_t value);
+uint64_t get_hash(uint8_t *data, size_t size);
 
+// work_with_folders.c:
 // Returns 1 if the folder exists, 0 if it doesn't
 int is_dir_exist(const char *folder);
 // Opens the given file, creates any missing paths of the path if needed
 FILE* fopen_no_matter_what(const char *filename, const char *access_mode);
 int remove_directory(const char *dir);
 
-void * alloc_memory(void *p, size_t num_elements, size_t sizeof_element);
